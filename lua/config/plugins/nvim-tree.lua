cbaab95457d224574413891417948c09ed553cc0
Brandon Card <bozo3887@gmail.com>
2025-03-22 19:26:58 -0400

    nvim-tree revert

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -1,23 +1,17 @@
 return {
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    lazy = false,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      require('nvim-tree').setup {
-        view = {
-          --          adaptive_size = true, -- Automatically adjusts the width to fit file names
-        },
-      }
-      -- Auto open nvim-tree when Neovim starts
-      vim.api.nvim_create_autocmd('VimEnter', {
-        callback = function()
-          require('nvim-tree.api').tree.open()
-        end,
-      })
-    end,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({
+				view = {
+					--          adaptive_size = true, -- Automatically adjusts the width to fit file names
+				},
+			})
+		end,
+	},
 }
