4beb2b6afa56fbb268adfd5f1108b187df9d27b3
Brandon Card <bozo3887@gmail.com>
2025-03-25 23:47:43 -0400

    feat: nvim-tree update

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -7,7 +7,65 @@ return {
       'nvim-tree/nvim-web-devicons',
     },
     config = function()
-      require('nvim-tree').setup {}
+      require('nvim-tree').setup {
+        sort = {
+          sorter = 'case_sensitive',
+        },
+        view = {
+          width = 30,
+          relativenumber = false,
+        },
+        renderer = {
+          group_empty = true,
+          highlight_modified = 'name',
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+          },
+        },
+        filters = {
+          dotfiles = false,
+        },
+        git = {
+          enable = true,
+          ignore = true,
+          timeout = 500,
+        },
+        diagnostics = {
+          enable = true,
+          show_on_dirs = true,
+          icons = {
+            hint = '',
+            info = '',
+            warning = '',
+            error = '',
+          },
+        },
+        modified = {
+          enable = true,
+          show_on_dirs = true,
+        },
+        actions = {
+          open_file = {
+            quit_on_open = false,
+            window_picker = {
+              enable = true,
+            },
+          },
+        },
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        },
+        filesystem_watchers = {
+          enable = true,
+          debounce_delay = 50,
+        },
+      }
     end,
   },
 }
