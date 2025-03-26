724969f13ddd1dbce2f42a826366c8379acc94a6
Brandon Card <bozo3887@gmail.com>
2025-03-25 23:58:47 -0400

    feat: nvim-tree opens at nvim start while keeping dashboard

--- init.lua
+++ init.lua
@@ -9,3 +9,16 @@ require 'config.lazy'
 -- (or you could consider 'lukas-reineke/indent-blankline.nvim')
 --------------------------------------------------------------------------------
 require 'kickstart.plugins.indent_line'
+
+-- Auto-open nvim-tree on startup while preserving dashboard
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    -- This delay ensures the dashboard loads first
+    vim.defer_fn(function()
+      -- Open the tree
+      require('nvim-tree.api').tree.open()
+      -- Return focus to the main window (dashboard)
+      vim.cmd 'wincmd p'
+    end, 100)
+  end,
+})
