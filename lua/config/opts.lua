5706e7fbfac680c1f28fec94e3597e86e5c5e9b6
Brandon Card <bozo3887@gmail.com>
2025-03-31 09:01:41 -0400

    feat(opts): Enhance Neovim configuration
    - Add performance improvements (updatetime, history)
    - Enhance visual feedback (termguicolors, showmatch)
    - Improve tab and indent settings
    - Add file handling enhancements
    - Clean up duplicate settings
    - Organize options into logical sections
    - Maintain original documentation structure
    
    Split-window substitution preview (inccommand='split') retained

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -12,69 +12,72 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
+-- Line numbers and relative numbers
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = false -- Helps with motion commands
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+-- Search settings
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
--- Show which line your cursor is on
-vim.opt.cursorline = true
+-- Tab and indent settings
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
 
+-- Visual enhancements
+vim.opt.termguicolors = true
+vim.opt.showmatch = true
+-- vim.opt.colorcolumn = '80'
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
+vim.opt.showmode = true
+vim.opt.cmdheight = 2
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'` and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+-- Performance and history
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
+vim.opt.history = 1000
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- File handling
+vim.opt.undofile = true
+vim.opt.undolevels = 1000
+vim.opt.autoread = true
 
--- set spell check
+-- Status line
+vim.opt.laststatus = 3
+
+-- Spell checking
 vim.opt.spell = true
 vim.opt.spelllang = 'en_us'
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+-- Clear search highlighting with <Esc>
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
