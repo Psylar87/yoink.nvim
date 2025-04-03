10f022afcff542ce14dc1b2de74d1460398ad79c
Brandon Card <bozo3887@gmail.com>
2025-04-03 00:14:45 -0400

    feat(yazi): Add directory shortcuts and improve configuration
    - Added convenient directory navigation shortcuts using Yazi file
    manager:
      - <leader>yh: Home directory
      - <leader>yn: Neovim config directory
      - <leader>yw: Downloads directory
      - <leader>yk: Desktop directory
      - <leader>yc: .config directory
    
    - Added popup window configuration with rounded borders and centered
    positioning
    - Added file preview settings with line numbers
    - Added FileType autocmd to disable line numbers in Yazi buffer

--- lua/config/plugins/yazi.lua
+++ lua/config/plugins/yazi.lua
@@ -3,34 +3,96 @@ return {
   {
     'mikavilpas/yazi.nvim',
     event = 'VeryLazy',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
     keys = {
-      -- 👇 in this section, choose your own keymappings!
       {
         '<leader>-',
         '<cmd>Yazi<cr>',
         desc = 'Open yazi at the current file',
       },
       {
-        -- Open in the current working directory
         '<leader>cw',
         '<cmd>Yazi cwd<cr>',
         desc = "Open the file manager in nvim's working directory",
       },
       {
-        -- NOTE: this requires a version of yazi that includes
-        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
         '<c-y>',
         '<cmd>Yazi toggle<cr>',
         desc = 'Resume the last yazi session',
       },
-    },
-    ---@type YaziConfig
-    opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
-      keymaps = {
-        show_help = '<f1>',
+      {
+        '<leader>yh',
+        function()
+          vim.cmd.lcd(vim.fn.expand '$HOME')
+          vim.cmd 'Yazi cwd'
+        end,
+        desc = 'Open yazi in home directory',
+      },
+      {
+        '<leader>yn',
+        function()
+          vim.cmd.lcd(vim.fn.stdpath 'config')
+          vim.cmd 'Yazi cwd'
+        end,
+        desc = 'Open yazi in neovim config directory',
+      },
+      {
+        '<leader>yw',
+        function()
+          vim.cmd.lcd(vim.fn.expand '$HOME/Downloads')
+          vim.cmd 'Yazi cwd'
+        end,
+        desc = 'Open yazi in Downloads directory',
+      },
+      {
+        '<leader>yk',
+        function()
+          vim.cmd.lcd(vim.fn.expand '$HOME/Desktop')
+          vim.cmd 'Yazi cwd'
+        end,
+        desc = 'Open yazi in Desktop directory',
+      },
+      {
+        '<leader>yc',
+        function()
+          vim.cmd.lcd(vim.fn.expand '$HOME/.config')
+          vim.cmd 'Yazi cwd'
+        end,
+        desc = 'Open yazi in .config directory',
       },
     },
+    opts = {
+      open_for_directories = false,
+      popup = {
+        size = {
+          width = '90%',
+          height = '80%',
+        },
+        position = '50%',
+        border = 'rounded',
+      },
+      keymaps = {
+        show_help = '<f1>',
+        parent_dir = 'h',
+        close = '<esc>',
+        refresh = '<c-r>',
+        toggle_hidden = '.',
+      },
+      preview = {
+        enabled = true,
+        show_line_numbers = true,
+      },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'yazi',
+        callback = function()
+          vim.opt_local.number = false
+          vim.opt_local.relativenumber = false
+        end,
+      })
+    end,
   },
 }
