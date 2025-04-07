afd6785899ea02103599193d62f84f2544e8a015
Brandon Card <bozo3887@gmail.com>
2025-04-06 21:21:19 -0400

    feat(plugins): configure img-clip.nvim with enhanced settings
    - Add smart directory structure for image storage
    - Configure relative paths and timestamps for file naming
    - Set up filetype-specific templates for Markdown and LaTeX
    - Add additional keymaps for image operations
    - Install pngpaste dependency for macOS support

--- lua/config/plugins/img-clip.lua
+++ lua/config/plugins/img-clip.lua
@@ -2,11 +2,47 @@ return {
   'HakonHarnes/img-clip.nvim',
   event = 'VeryLazy',
   opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
+    default = {
+      dir_path = function()
+        return vim.fn.expand '%:p:h' .. '/images'
+      end,
+      extension = 'png',
+      file_name = '%Y%m%d-%H%M%S',
+      use_absolute_path = false,
+      relative_to_current_file = true,
+      verbose = true,
+      insert_mode_after_paste = true,
+      prompt_for_file_name = true,
+      show_dir_path_in_prompt = true,
+      download_images = true,
+    },
+    filetypes = {
+      markdown = {
+        url_encode_path = true,
+        template = '![$CURSOR]($FILE_PATH)',
+      },
+      tex = {
+        template = [[
+\begin{figure}[htbp]
+  \centering
+  \includegraphics[width=0.8\textwidth]{$FILE_PATH}
+  \caption{$CURSOR}
+  \label{fig:$LABEL}
+\end{figure}
+        ]],
+      },
+    },
   },
   keys = {
-    -- suggested keymap
-    { '<leader>p', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
+    { '<leader>p', '<cmd>PasteImage<cr>', desc = 'Paste image from clipboard' },
+    {
+      '<leader>P',
+      function()
+        require('img-clip').paste_image { use_absolute_path = true }
+      end,
+      desc = 'Paste image with absolute path',
+    },
+    { '<leader>id', '<cmd>ImgClipDebug<cr>', desc = 'Show img-clip debug info' },
+    { '<leader>ic', '<cmd>ImgClipConfig<cr>', desc = 'Show img-clip config' },
   },
 }
