b929147a84a393b1188ca6a17090ca02d4fdf052
Brandon Card <bozo3887@gmail.com>
2025-04-08 11:49:31 -0400

    feat(lsp): add nvim-cmp integration and fix LSP dependencies
    - Added nvim-cmp and related plugins for completion support
    - Integrated completion engine with LSP capabilities
    - Added snippet support via LuaSnipr
    - Configured completion sources and keymaps

--- lua/config/plugins/lsps.lua
+++ lua/config/plugins/lsps.lua
@@ -6,22 +6,68 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
       'saghen/blink.cmp',
-      -- Automatically install LSPs and related tools
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-
-      -- Active fork of Mason tool installer:
-      -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      -- Useful status updates for LSP (archived but still works if you like it)
-      -- Alternative: https://github.com/folke/noice.nvim or other UI plugins
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Improved support for setting up Lua development in Neovim configs
       { 'folke/neodev.nvim', opts = {} },
+      -- Completion Dependencies
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
     },
     config = function()
+      ----------------------------------------------------------------------------
+      -- Completion Setup
+      ----------------------------------------------------------------------------
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+      })
+
       ----------------------------------------------------------------------------
       --  Setup LSP-on-attach Keymappings
       ----------------------------------------------------------------------------
@@ -32,6 +78,7 @@ return {
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
+          -- Keymaps
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
@@ -43,8 +90,18 @@ return {
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-          -- Highlight references when cursor hovers on a symbol
+          -- Format on save
           local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = event.buf,
+              callback = function()
+                vim.lsp.buf.format({ buffer = event.buf })
+              end,
+            })
+          end
+
+          -- Document highlight
           if client and client.server_capabilities.documentHighlightProvider then
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
@@ -60,42 +117,60 @@ return {
       })
 
       ----------------------------------------------------------------------------
-      --  Extend LSP Capabilities (Autocompletion, Snippets, etc.)
+      --  Extend LSP Capabilities
       ----------------------------------------------------------------------------
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       ----------------------------------------------------------------------------
-      --  Servers to Enable / Configure
+      --  Servers Configuration
       ----------------------------------------------------------------------------
       local servers = {
         -- Rust
-        rust_analyzer = {},
-        -- SourceKit (Swift)
-        require('lspconfig').sourcekit.setup {
-          cmd = { '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp' },
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              checkOnSave = {
+                command = "clippy",
+              },
+            },
+          },
         },
         -- Lua
         lua_ls = {
           settings = {
             Lua = {
               completion = { callSnippet = 'Replace' },
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                globals = { 'vim' },
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
             },
           },
         },
-        -- Nix (requires `nixd` to be installed, done via nix-darwin in my case)
-        -- this could be better, but something is not working somewhere for me
-        -- when adding the options {} table to the setup call
-        -- TODO: Revist later
-        require('lspconfig').nixd.setup {
-          cmd = { 'nixd' },
+        -- Bash
+        bashls = {},
+        -- Go
+        gopls = {
           settings = {
-            nixd = {
-              nixpkgs = {
-                expr = 'import <nixpkgs> { }',
+            gopls = {
+              analyses = {
+                unusedparams = true,
               },
-              formatting = {
-                command = { 'alejandra' },
+              staticcheck = true,
+            },
+          },
+        },
+        -- Python
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "basic",
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
               },
             },
           },
@@ -105,25 +180,33 @@ return {
       ----------------------------------------------------------------------------
       --  Mason Setup
       ----------------------------------------------------------------------------
-      require('mason').setup()
+      require('mason').setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
 
-      -- Install the servers above (plus any extra tools you want)
       require('mason-tool-installer').setup {
         ensure_installed = {
-          -- These names match the Mason package names, so adjust as needed:
-          -- 'rust-analyzer', 'lua-language-server', etc.
-          'rust-analyzer',
+          'bash-language-server',
           'lua-language-server',
-          'stylua', -- e.g. for Lua formatting
+          'rust-analyzer',
+          'gopls',
+          'pyright',
+          'stylua',
+          'debugpy',
+          'delve',
         },
         auto_update = false,
         run_on_start = true,
       }
 
       require('mason-lspconfig').setup {
-        -- Make sure these server names match the table keys in `servers`
         ensure_installed = vim.tbl_keys(servers),
-
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
