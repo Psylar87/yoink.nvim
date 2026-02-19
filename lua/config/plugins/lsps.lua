31f14fb969c43a1e987315f1b622ac50e0bb646e
Brandon Card <bozo3887@gmail.com>
2026-02-19 13:14:36 -0500

    fix(lsps): add lua_ls runtime and diagnostics settings

--- lua/config/plugins/lsps.lua
+++ lua/config/plugins/lsps.lua
@@ -92,7 +92,17 @@ return {
         lua_ls = {
           settings = {
             Lua = {
+              runtime = {
+                version = 'LuaJIT',
+              },
+              diagnostics = {
+                globals = { 'vim' },
+              },
               completion = { callSnippet = 'Replace' },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false,
+              },
             },
           },
         },
